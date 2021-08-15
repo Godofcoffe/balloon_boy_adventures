bb0265795baf6832bc3619fcad64443eddb8b9a3
Godofcoffe <robotnyte@gmail.com>
2021-08-15 19:22:25 -0300

    pre-alpha

---
+++ scripts/controles.js
@@ -0,0 +1,31 @@
+const bb = document.getElementById("personagem")
+let posx = 600
+let posy = 505
+let vel = 10
+function keyPressed(evt){
+    evt = evt || window.event;
+    let key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+function pseudo_sleep() {
+    return null
+}
+
+document.onkeypress = function(evt) {
+    let tecla = keyPressed(evt);
+    if (tecla == "d") {
+        posx += vel
+        bb.style.transform = "rotateY(0)"
+        bb.style.marginLeft = `${posx}px`
+    }
+    if (tecla == "a") {
+        posx -= vel
+        bb.style.marginLeft = `${posx}px`
+        bb.style.transform = "rotateY(180deg)"
+    }
+    if (tecla == "w") {
+        bb.style.animation = ""
+        bb.style.animation = "salto 2s"
+    }
+}
---
+++ scripts/motor.js
@@ -0,0 +1,1 @@
+const tela = document.body
